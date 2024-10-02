8df361c44f64a76fc2b4d0b7c9533f5d294d3913
l3vith <smitbarve666@gmail.com>
2024-10-02 13:52:48 +0530

    Changed Logo Color

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -21,9 +21,9 @@ export const Sidebar = ({ className }: SidebarProps) => {
     >
       <Link href="/learn">
         <div className="flex items-center gap-x-3 pb-7 pl-4 pt-8">
-          <Image src="/mascot.svg" alt="Mascot" height={40} width={40} />
+          <Image src="/logoblue.png" alt="Mascot" height={40} width={40} />
 
-          <h1 className="text-2xl font-extrabold tracking-wide text-green-600">
+          <h1 className="text-2xl font-extrabold tracking-wide text-customColor">
             Signify
           </h1>
         </div>
