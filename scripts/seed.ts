d00f95674a002e2ef6155dc28dac079c93ea1f98
l3vith <smitbarve666@gmail.com>
2024-10-03 23:03:26 +0530

    fixed minor typo

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -19,7 +19,7 @@ const main = async () => {
         await db.delete(schema.units);
         await db.delete(schema.lessons);
         await db.delete(schema.challenges);
-        await db.delete(schema.challengesOption);
+        await db.delete(schema.challengeOptions);
         await db.delete(schema.challengeProgress);
 
         await db.insert(schema.courses).values([
